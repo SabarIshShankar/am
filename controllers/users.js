2eadb530eabac92d721531dc6033bd3c6ab6ed39
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-26 11:07:27 +0000

    u

--- controllers/users.js
+++ controllers/users.js
@@ -39,3 +39,77 @@ exports.login = async(req, res) => {
 		})
 	}
 }
+
+exports.register = async(req, res) => {
+	try{
+		const exisitingUser = await User.find({email: req.body.email}).limit(1)
+		if(existingUser.length > 0){
+			retrun res.status(409).json({
+				message: 'Email already exisits'
+			})
+		}
+		const {name, email, password} = req.body
+		const hash = await bcrypt.hash(password, 10)
+		const user = await new User({
+			name, email, password: hash
+		}).save()
+		return res.status(201).json({
+			message: 'account created. please login'
+		})
+	}
+	catch(err){
+		console.log(err)
+		return res.status(500).json({
+			SOMETHING_WENT_WRONG: 'Try again'
+		})
+	}
+}
+
+exports.logout = (req, res) => {
+	try{
+		res.clearCookie("refresh_token_video_conf");
+		return res.status(200).json({
+			message: 'logged out'
+		})
+	}
+	catch(err){
+		return res.status(500).json({
+			message: 'Something went wrong'
+		})
+	}
+}
+
+exports.refresh = async(req, res) => {
+	try{
+		if(!req.cookies.refresh_token_video_conf){
+			return res.status(401).json({
+				message: 'Auth failed'
+			})
+		}
+
+		const {id, name} = jwt.verify(req.cookies.refresh_token_video_conf, process.env.REFRESHTOKENKEY)
+		const access_token = jwt.sign({id, name}, process.env.SECRETKEY, {
+			expiresIn: '600s'
+		})
+		if(req.body.getProfile){
+			const profile = await User.findById(id)
+			if(profile === null){
+				return res.status(404).json({
+					message: 'Account not found'
+				})
+			}
+			return res.status(200).json({
+				profile, access_token
+			})
+		}
+		return res.status(200).json({
+			access_token
+		})
+	}
+	catch(err){
+		console.log(err)
+		return res.status(401).json({
+			message: 'Authorization failed'
+		})
+	}
+}
