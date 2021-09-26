42dbcbab75c32262d80d549ed1d1f91504e2840c
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-26 11:14:41 +0000

    u config

---
+++ config/jwt.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+exports.verifyJwt = token => {
+	const decoded = jwt.verify(token, process.env.SECRETKEY);
+	return decoded
+}
+
+exports.extractJwt = () => {
+	const token = req.headers.authorization.split(' ')[1];
+	return this.verifyJwt(token)
+}
+
+exports.generateTokens = async(id, name) => {
+	return await Promise.all([
+		jwt.sign({
+			id, name
+		}, process.env.SECRETKEY, {
+			expiresIn: '6000s'
+		}), jwt.Sign({id, name}, process.env.REFRESHTOKENKEY, {
+			expiresIn: 15778800000
+		})
+	])
+}
